8562c51a7e90010cc635c78bd45bf4703298d3da
KaboSD <signkabob@gmail.com>
2026-08-10 12:50:34 -0500

    Created AudioManager.cs and SettingUI.cs

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,60 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+/*
+ * Excercise 03.4: AudioManager.cs
+ * Name: Ka Bo Cheung
+ * Date: 08/01/2026
+ * Course: GAME-1377-001
+ *
+ * Script for the audio manager
+ */
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance {  get; private set; }
+
+
+    public AudioSource MusicSource;
+    public AudioSource SFXSource;
+    
+    [SerializeField] private AudioClip backgroundMusic;
+    
+
+    void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (backgroundMusic != null)
+        {
+            PlayBackgroundMusic();
+        }
+    }
+
+    public void GetCurrentVolume(out float currentMusicVolume, out float currentSFXVolume)
+    {
+        currentMusicVolume = MusicSource.volume;
+        currentSFXVolume = SFXSource.volume;
+    }
+
+    /// <summary>
+    /// Plays the background music
+    /// </summary>
+    private void PlayBackgroundMusic()
+    {
+        MusicSource.clip = backgroundMusic;
+        MusicSource.loop = true;
+        MusicSource.Play();
+    }
+
+}
---
+++ Assets/Scripts/GeneralUI/SettingUI.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+/*
+ * Excercise 03.4: SettingUI.cs
+ * Name: Ka Bo Cheung
+ * Date: 08/01/2026
+ * Course: GAME-1377-001
+ *
+ * Script for the setting UI controls and modifiers
+ */
+public class SettingUI : MonoBehaviour
+{
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    void Start()
+    {
+        AudioManager.Instance.GetCurrentVolume(out float currentMusicVolume,  out float currentSFXVolume);
+        musicSlider.value = currentMusicVolume;
+        SFXSlider.value = currentSFXVolume;
+    }
+    
+    /// <summary>
+    /// Change the music volume to specific ratio
+    /// </summary>
+    /// <param name="amount">Ratio of the volume</param>
+    public void SetMusicVolume(float amount)
+    {
+        AudioManager.Instance.MusicSource.volume = amount;
+    }
+
+    /// <summary>
+    /// Change the SFX volume to specfic ratio 
+    /// </summary>
+    /// <param name="amount">Ratio of the volume</param>
+    public void SetSFXVolume(float amount)
+    {
+        AudioManager.Instance.SFXSource.volume = amount;
+    }
+}
